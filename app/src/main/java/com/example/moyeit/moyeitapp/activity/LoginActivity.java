f60d910f1071c8c70590fd4a357ef17ed9def42c
wjddpdms9 <wjddpdms9@gmail.com>
2017-08-13 14:40:14 +0900

    마이리스트

--- app/src/main/java/com/example/moyeit/moyeitapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/moyeit/moyeitapp/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login);
 
         editId = (AutoCompleteTextView) findViewById(R.id.editText_id);
